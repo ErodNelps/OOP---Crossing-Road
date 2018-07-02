d253b2c3ce32ac73d4ec9373580b62e7676f3d2c
ErodNelps <40132636+ErodNelps@users.noreply.github.com>
2018-07-02 19:20:41 +0700

    Add files via upload

--- Class.h
+++ Class.h
@@ -1,8 +1,26 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+#define WHITE 15
+#define GREEN 2
+#define RED 4
+#define CYAN 3
+#define YELLOW 14
+#define LIGHTGREEN 10
+#define LIGHTMAGNETA 13
+#define LIGHTGRAY 7
+#define DARKGRAY 8
 #include <iostream>
 #include <Windows.h>
 #include <thread>
 #include <conio.h>
+#include <string>
+#include <fstream>
+#include <processthreadsapi.h>
+#include <vector>
+
+
+void gotoxy(int x, int y);
+void setTextAttribute(int k);
+void menu();
 
 
 using namespace std;
@@ -12,10 +30,7 @@ class CPEOPLE {
 	bool mState; //Trạng thái sống chết
 public:
 	CPEOPLE();
-	void Up(int);
-	void Left(int);
-	void Right(int);
-	void Down(int);
+	Control();
 	bool isImpact(const CVEHICLE*&);
 	bool isImpact(const CANIMAL*&);
 	bool isFinish();
@@ -25,13 +40,14 @@ public:
 class CVEHICLE {
 	int mX, mY;
 public:
-	virtual void Move(int, int);
-	//…
+
+	virtual void Move();
 };
 
 class CTRUCK : public CVEHICLE {
 public:
-	//…
+	void Move()
+
 };
 
 class CCAR : public CVEHICLE {
@@ -42,7 +58,8 @@ public:
 class CANIMAL {
 	int mX, mY;
 public:
-	virtual void Move(int, int);
+	virtual void Move(int X, int Y) {
+	};
 	virtual void Tell();
 };
 
@@ -57,14 +74,20 @@ public:
 };
 
 class CGAME {
+	int width;
+	int height;
 	CTRUCK* axt;
 	CCAR* axh;
 	CDINAUSOR* akl;
 	CBIRD* ac;
 	CPEOPLE cn;
 public:
-	CGAME(); //Chuẩn bị dữ liệu cho tất cả các đối tượng
-	void drawGame(); //Thực hiện vẽ trò chơi ra màn hình sau khi có dữ liệu
+	CGAME(int width, int height)
+	{
+		this->width = width;
+		this->height = height;
+	}//Chuẩn bị dữ liệu cho tất cả các đối tượng
+	void drawGame(int x, int y, int width, int height, int color, string textinbutton); //Thực hiện vẽ trò chơi ra màn hình sau khi có dữ liệu
 	~CGAME(); // Hủy tài nguyên đã cấp phát
 	CPEOPLE getPeople();//Lấy thông tin người
 	CVEHICLE* getVehicle();//Lấy danh sách các xe
@@ -72,11 +95,108 @@ public:
 	void resetGame(); // Thực hiện thiết lập lại toàn bộ dữ liệu như lúc đầu
 	void exitGame(HANDLE); // Thực hiện thoát Thread
 	void startGame(); // Thực hiện bắt đầu vào trò chơi
-	//void loadGame(istream& is); // Thực hiện tải lại trò chơi đã lưu
-	//void saveGame(istream& is); // Thực hiện lưu lại dữ liệu trò chơi
-	void pauseGame(HANDLE); // Tạm dừng Thread
-	void resumeGame(HANDLE); //Quay lai Thread
+	void loadGame(istream& is); // Thực hiện tải lại trò chơi đã lưu
+	void saveGame(istream& is); // Thực hiện lưu lại dữ liệu trò chơi
+	void pauseGame(HANDLE h) {
+		SuspendThread(h);
+	} // Tạm dừng Thread
+	void resumeGame(HANDLE h) {
+		ResumeThread(h);
+	}; //Quay lai Thread
 	void updatePosPeople(char); //Thực hiện điều khiển di chuyển của CPEOPLE
 	void updatePosVehicle(); //Thực hiện cho CTRUCK & CCAR di chuyển
 	void updatePosAnimal();//Thực hiện cho CDINAUSOR & CBIRD di chuyển
 };
+
+CPEOPLE CGAME::getPeople()
+{
+	return cn;
+}
+
+CVEHICLE* CGAME::getVehicle()
+{
+}
+
+void gotoxy(int x, int y)
+{
+	static HANDLE  h = NULL;
+	if (!h)
+		h = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD c = { x, y };
+	SetConsoleCursorPosition(h, c);
+}
+
+void setTextAttribute(int k)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, k);
+}
+
+
+
+void CGAME::drawGame(int x, int y, int width, int height, int color, string textinbutton)
+{
+	setTextAttribute(color);
+	gotoxy(x - width, y - width);
+	cout << char(218);	//  ┌
+
+	for (int i = 0; i < height; i++)
+		cout << char(196);	//  ─
+	cout << char(191);	//  ┐
+
+	for (int i = 1 - width; i <= width - 1; i++)
+	{
+		gotoxy(x - width, y + i);
+		cout << char(179);	//  │ 
+	}
+
+	gotoxy(x - width, y + width);
+	cout << char(192);
+
+	for (int i = 0; i < height; i++)
+		cout << char(196);	//  ─
+	cout << char(217);	//  ┘
+
+	for (int i = 1 - width; i <= width - 1; i++)
+	{
+		gotoxy(x + height + 1 - width, y + i);
+		cout << char(179);	//  │ 
+	}
+
+	int center = (height - textinbutton.length()) / 2;
+	gotoxy(x + center, y);
+	cout << textinbutton;
+}
+
+void CGAME::saveGame(istream &is)
+{
+	fstream save;
+	save.open("Savefile.txt", fstream::out);
+	save << "..." << endl;
+}
+
+void CGAME::loadGame(istream &is)
+{
+	fstream load;
+	load.open("Savefile.txt", fstream::in);
+}
+
+void menu()
+{
+	setTextAttribute(3);
+	cout << " ::::::  :::::::   ::::::   ::::::   ::::::  :::::::  ::::  :::  :::::: " << endl;
+	cout << ":+:  :+: :+:  :+: :+:  :+: :+:  :+: :+:  :+:   :+:    :+:+  :+: :+:  :+:" << endl;
+	cout << "+:+      +:+  +:+ +:+  +:+ +:+      +:+        +:+    :+:+  +:+ +:+      " << endl;
+	cout << "+#+      +#+:+#:  +#+  +:+ +#++:+#+ +#++++#+   +#+    +#+++ +#+ :#:      " << endl;
+	cout << "+#+      +#+  +#+ +#+  +#+      +#+      +#+   +#+    +#+  +#+# +#+ +#+#" << endl;
+	cout << "#+#  #+# #+#  #+# #+#  #+# #+#  #+# #+#  #+#   #+#    #+#  +#+# #+#  #+#" << endl;
+	cout << " ######  ###  ###  ######   ######   ######  #######  ###   ###  ###### " << endl;
+
+	cout << ":::::::   ::::::     :::    :::::::" << endl;
+	cout << ":+:  :+: :+:  :+:   :+ +:   :+:  :+:" << endl;
+	cout << "+:+  +:+ +:+  +:+  +:+ +:+  +:+  +:+" << endl;
+	cout << "+#+:+#:  +#+  +:+ +#++:+#++ +#+  +:+" << endl;
+	cout << "+#+  +#+ +#+  +#+ +#+   +#+ +#+  +#+" << endl;
+	cout << "#+#  #+# #+#  #+# #+#   #+# #+#  #+#" << endl;
+	cout << "###  ###  ######  ###   ### ####### " << endl;
+}
