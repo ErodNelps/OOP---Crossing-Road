741ebc71d71388f479bcc06fecb7caeeec6781b6
ErodNelps <40132636+ErodNelps@users.noreply.github.com>
2018-07-25 23:45:10 +0700

    Update LoadingScreen.cpp

--- LoadingScreen.cpp
+++ LoadingScreen.cpp
@@ -28,4 +28,21 @@ void LoadingScreeen() {
 		else cout << "\tCompleted";
 		this_thread::sleep_for(chrono::milliseconds(70));
 	}
+	/*for (int i = 1; i <= 50; i++)
+	{
+		system("cls");
+		cout << "\n\n\n\n\t\t\t\t   Loading " << '\n' << '\t' << '\t';
+		for (int j = 1; j <= i; j++)
+			cout << char(219);
+		cout << "\n\n\t " << 2 * i << "%";
+		if (i > 1 && i < 20)
+			cout << "\n\n\tCreating Temporary files";
+		else if (i > 20 && i<40)
+			cout << "\n\n\tAccessing Main Memory";
+		else if (i >40 && i<48)
+			cout << "\n\n\tAccessing Cache";
+		else cout << "\n\n\tComplete. Press Enter to Continue ";
+		this_thread::sleep_for(chrono::milliseconds(100));
+	}
+	_getch();*/
 }
