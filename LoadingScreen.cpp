3a4e6d031d0f43c498a1eba2506e5057d17a3ee8
ErodNelps <40132636+ErodNelps@users.noreply.github.com>
2018-07-26 12:28:33 +0700

    Update LoadingScreen.cpp

--- LoadingScreen.cpp
+++ LoadingScreen.cpp
@@ -2,12 +2,11 @@ void LoadingScreeen() {
 	int x = 35;
 	int count = x;
 	int y = 12;
-	int width = 40;
+	int width = 50;
 	gotoxy(x, y - 2);
 	setTextAttribute(LIGHTGREEN);
-	for (int i = 1; i <= 40; i++)
+	for (int i = 1; i <= 50; i++)
 	{
-		system("cls");
 		drawborder(x, y, 1, width, LIGHTGREEN, " ");
 		gotoxy(x, y - 2);
 		cout << "Loading...";
@@ -19,13 +18,33 @@ void LoadingScreeen() {
 		gotoxy(x + 11, y - 2);
 		cout << 2 * i << "%";
 		gotoxy(x, y + 2);
+		cout << "                                        ";
+		gotoxy(x, y + 2);
 		if (i > 1 && i < 20)
 			cout << "\tCreating Temporary files";
 		else if (i > 20 && i < 30)
 			cout << "\tAccessing Main Memory";
-		else if (i > 30 && i < 38)
+		else if (i > 40 && i < 48)
 			cout << "\tHacking System";
 		else cout << "\tCompleted";
 		this_thread::sleep_for(chrono::milliseconds(70));
 	}
+	this_thread::sleep_for(chrono::milliseconds(700));
+}
+
+void ClearScreen() {
+    COORD topLeft  = { 0, 0 };
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(
+        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    FillConsoleOutputAttribute(
+        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    SetConsoleCursorPosition(console, topLeft);
 }
